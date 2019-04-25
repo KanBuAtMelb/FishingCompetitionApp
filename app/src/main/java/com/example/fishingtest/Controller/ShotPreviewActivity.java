00f8dbded8fade7701d693beee2a6fdcc72edad4
RoryKirsi <qq441072110@gmail.com>
2019-04-25 14:50:26 +1000

    post preview

---
+++ app/src/main/java/com/example/fishingtest/Controller/ShotPreviewActivity.java
@@ -0,0 +1,15 @@
+package com.example.fishingtest.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fishingtest.R;
+
+public class ShotPreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shot_preview);
+    }
+}
